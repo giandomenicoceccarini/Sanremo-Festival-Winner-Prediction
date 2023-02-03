bf147a448a589babb70d31aaef3887a9a2fac289
giando <gdceccarini@gmail.com>
2023-02-03 20:20:04 +0100

    Update unittest

--- test/test_insta_crawler.py
+++ test/test_insta_crawler.py
@@ -1,9 +1,10 @@
-from unittest import TestCase
+from unittest import TestCase, skip
 
 from src.crawler.instagram import InstaCrawler
 
 
 class TestInstaCrawler(TestCase):
+    @skip
     def test_get_follower(self):
         follower = InstaCrawler.get_follower(person_name="Anna Oxa")
         self.assertIsNotNone(follower)
--- test/test_wikicrawler.py
+++ test/test_wikicrawler.py
@@ -1,9 +1,10 @@
-from unittest import TestCase
+from unittest import TestCase, skip
 
 from src.crawler.wikipedia import WikiCrawler
 
 
 class TestWikiCrawler(TestCase):
+    @skip
     def test_get_info(self):
         info = WikiCrawler.get_artist_info(person_name="Marco Mengoni")
         self.assertIsNotNone(info)
